7a46e6ff81377d7662e79657163496770878d4d0
kei-yagi <kei_y@funteractive.co.jp>
2024-12-26 14:00:02 +0900

    feat: add Emoji perspective interaction

---
+++ app/emoji/page.tsx
@@ -0,0 +1,9 @@
+import { Emoji } from '@/features/routes/emoji/components/Emoji';
+
+export default function EmojiPage() {
+  return (
+    <div className="h-[100svh]">
+      <Emoji />
+    </div>
+  );
+}
